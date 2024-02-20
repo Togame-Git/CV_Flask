bb693bbe9471635909bf5af448f3fa3049c1f834
Togame-Git <wendykuntz.pro@gmail.com>
2024-02-20 15:35:06 +0100

    update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template
-from flask import Flask, render_template, jsonify
-import json
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import Flask, render_template, request, redirect
+from flask import json
+from urllib.request import urlopen
 import sqlite3
 
 app = Flask(__name__) #creating flask app name
