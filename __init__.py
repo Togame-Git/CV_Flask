89f965ffeb8265b8aeb1bac8b1205d44cd5ca02a
Togame-Git <wendykuntz.pro@gmail.com>
2024-02-20 15:35:50 +0100

    update route

--- __init__.py
+++ __init__.py
@@ -35,5 +35,17 @@ def ReadBDD():
     # Renvoie la réponse JSON
     return jsonify(posts=json_posts)
 
+# Création d'une nouvelle route pour la lecture de la BDD
+@app.route("/consultation/")
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
 if(__name__ == "__main__"):
     app.run()
